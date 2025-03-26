44fa42858f53cd30ecc65968c1722ac566478f9a
HelenCrystina <helenandradee0807@gmail.com>
2025-03-26 10:45:48 -0300

    Adicinando calculo

--- Index.js
+++ Index.js
@@ -1,5 +1,5 @@
 //Realizando operações Math
 
 console.log(Math.pow(7, 2));
-console.log(Math.sqrt(10));
-console.log(Math.max([45[56]]))
+console.log(Math.sqrt(100));
+console.log(Math.max([45[56]]));
